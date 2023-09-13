24103bb2c012258abfdc263580fddb62488d66c2
Gregory Van Horne <gvanhorne@telus.net>
2023-09-13 13:46:02 -0700

    Add Host and Connection headers

--- SmartClient.py
+++ SmartClient.py
@@ -2,18 +2,35 @@ import urllib.request
 import sys
 
 def send_get_request(url):
+    headers = {
+        'Host': 'www.example.com',
+        'Connection': 'Keep-Alive',
+    }
     try:
-        response = urllib.request.urlopen(url)
-        return response.read().decode('utf-8')
+        request = urllib.request.Request(url, headers=headers, method='GET')
+        response = urllib.request.urlopen(request)
+        return request, response
     except urllib.error.URLError as e:
-        return str(e)
+        return None, str(e)
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
-        print("Usage: python get_request_example.py <URL>")
+        print("Usage: python3 SmartClient.py <URL>")
         sys.exit(1)
 
     url = sys.argv[1]
 
-    result = send_get_request(url)
-    print(result)
+    request, response = send_get_request(url)
+
+    if request is not None:
+        print(f"{request.get_method()} {request.full_url}")
+        
+        # Print any request headers if needed
+        for header, value in request.headers.items():
+            print(f"{header}: {value}")
+
+    if response is not None:
+        content = response.read().decode('utf-8')
+        print(content)
+    else:
+        print("Error occurred while making the request.")
