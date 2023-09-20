3273a88e1e96c59e0c7402b2b58323e655d463ef
Gregory Van Horne <gvanhorne@telus.net>
2023-09-20 13:36:49 -0700

    Parse URL to check for scheme and path

--- SmartClient.py
+++ SmartClient.py
@@ -1,17 +1,7 @@
 import socket
 import ssl
 import sys
-def print_response(response):
-    """
-    Print the HTTP response received from the server.
-
-    Parameters:
-    response (bytes): The response data to be printed.
-
-    Returns:
-    None
-    """
-    print(response.decode("utf-8"))
+from urllib.parse import urlparse
 
 def send_get_request(url):
     """
@@ -23,23 +13,40 @@ def send_get_request(url):
     Returns:
     None
     """
+    # Parse the URL
+    parsed_url = urlparse(url)
+    scheme = parsed_url.scheme
+    # Ensure the scheme is "http://" if it's missing
+    if not scheme:
+        parsed_url = urlparse(f"http://{url}", scheme='http')
+    path = parsed_url.path if parsed_url.path else "/"
+    hostname = parsed_url.netloc
+
     # Create an SSL context to establish a secure connection
     context = ssl.create_default_context()
 
     # Create a socket and wrap it with SSL, specifying the server's hostname
-    conn = context.wrap_socket(socket.socket(socket.AF_INET), server_hostname=url)
+    conn = context.wrap_socket(socket.socket(socket.AF_INET), server_hostname=hostname)
 
     try:
         # Connect to the server on port 443 (HTTPS)
         conn.connect((url, 443))
 
         # Send an HTTP GET request to the server
-        request = b"GET / HTTP/1.1\r\n\r\n"
+        # Construct the HTTP GET request with the 'Connection: Keep-Alive' header
+        print('---Request Begin---')
+        request = b"GET " + path.encode() + b" HTTP/1.1\r\n"
+        request += b"Host: " + hostname.encode() + b"\r\n"
+        request += b"Connection: Keep-Alive\r\n\r\n"
+        print(request.decode("utf-8")[:-1])
         conn.send(request)
+        print('---Request End---')
+        print('HTTP request sent, awaiting response. . .\n')
 
         # Receive and print the server's response (up to 1024 bytes)
+        print('---Response Header---')
         response = conn.recv(1024)
-        print_response(response)
+        print(response.decode("utf-8"))
     except Exception as e:
         print(f"An error occurred: {e}")
     finally:
