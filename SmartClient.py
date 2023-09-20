a5e3dd6cdd28fd8da3ae71fbfc1cf01fb3f59f45
Gregory Van Horne <gvanhorne@telus.net>
2023-09-20 12:21:17 -0700

    Add docstring and inline comments

--- SmartClient.py
+++ SmartClient.py
@@ -3,16 +3,37 @@ import ssl
 import sys
 
 def send_get_request(url):
+    """
+    Send an HTTP GET request to a given URL and print the response.
+
+    Parameters:
+    url (str): The URL to which the request will be sent.
+
+    Returns:
+    None
+    """
+    # Create an SSL context to establish a secure connection
     context = ssl.create_default_context()
+
+    # Create a socket and wrap it with SSL, specifying the server's hostname
     conn = context.wrap_socket(socket.socket(socket.AF_INET), server_hostname=url)
 
-    conn.connect((url, 443))
+    try:
+        # Connect to the server on port 443 (HTTPS)
+        conn.connect((url, 443))
 
-    conn.send(b"GET / HTTP/1.1\r\n\r\n")
+        # Send an HTTP GET request to the server
+        request = b"GET / HTTP/1.1\r\n\r\n"
+        conn.send(request)
 
-    conn.send(b"GET / HTTP/1.1\r\n\r\n")
-
-    print(conn.recv(1024))
+        # Receive and print the server's response (up to 1024 bytes)
+        response = conn.recv(1024)
+        print(response)
+    except Exception as e:
+        print(f"An error occurred: {e}")
+    finally:
+        # Close the connection when done
+        conn.close()
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
